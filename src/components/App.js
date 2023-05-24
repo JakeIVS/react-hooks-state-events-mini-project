d5fd3ce51441f3460caaa32994668027a2b2353d
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-24 16:31:34 -0600

    category filters work, don't update button class yet

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,25 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
-  const [filteredTasks, setFilteredTasks] = useState(TASKS) 
+  const [currentTasks, setCurrentTasks] = useState(TASKS) 
+  const [catSelect, setCatSelect] = useState("All")
+
   function handleDelete(item) {
-    setFilteredTasks(filteredTasks.filter(task=>(task.text !== item)))
+    setCurrentTasks(currentTasks.filter(task=>(task.text !== item)))
   }
+
+  function handleCatChange(cat) {
+    setCatSelect(cat)
+  }
+  
+  let filteredTasks = currentTasks.filter(task=>(
+    catSelect === "All" ? true : task.category === catSelect
+  ))
 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} onCatChange={handleCatChange} />
       <NewTaskForm />
       <TaskList tasks={filteredTasks} onDelete={handleDelete} />
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onCatChange }) {
+
+  const catBtns = categories.map(cat=>(
+    <button key={cat} onClick={()=>onCatChange(cat)} >{cat}</button>
+    ))
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {catBtns}
     </div>
   );
 }
