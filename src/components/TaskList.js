c772e8629f7594bb272d3ec8677cc66e2fd3e5df
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-24 15:49:12 -0600

    delete button works

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import Task from "./Task";
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, onDelete }) {
   return (
     <div className="tasks">
       {tasks.map(task=>(
-        <Task key={task.text} text={task.text} category={task.category} />
+        <Task key={task.text} text={task.text} category={task.category} onDelete={onDelete} />
       ))}
     </div>
   );
